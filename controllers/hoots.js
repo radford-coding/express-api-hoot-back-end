584f4dc76e0814ec528251ad75d809080ddd8bea
Matt Radford <quinquex@gmail.com>
2025-03-17 09:04:19 -0700

    define hoots router and link up

---
+++ controllers/hoots.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const verifyToken = require('../middleware/verify-token');
+const Hoot = require('../models/hoot');
+const router = express.Router();
+
+// routes
+
+module.exports = router;
