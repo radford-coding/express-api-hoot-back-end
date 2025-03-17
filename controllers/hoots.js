bb6238f22a4af6cb80e43d0ad5718679728c249d
Matt Radford <quinquex@gmail.com>
2025-03-17 09:59:10 -0700

    POST and GET /hoots

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -3,6 +3,28 @@ const verifyToken = require('../middleware/verify-token');
 const Hoot = require('../models/hoot');
 const router = express.Router();
 
-// routes
+// POST /hoots
+router.post('/', verifyToken, async (req, res) => {
+    try {
+        req.body.author = req.user._id;
+        const hoot = await Hoot.create(req.body);
+        hoot._doc.author = req.user;
+        res.status(201).json(hoot);
+    } catch (error) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// GET /hoots
+router.get('/', verifyToken, async (req, res) => {
+    try {
+        const hoots = await Hoot.find({})
+            .populate('author')
+            .sort({ createdAt: 'desc' });
+        res.status(200).json(hoots);
+    } catch (error) {
+        res.status(500).json({ err: err.message });
+    }
+});
 
 module.exports = router;
